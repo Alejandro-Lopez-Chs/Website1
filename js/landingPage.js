9e50049c2bf2080d6cb3fe6f86376c504168271b
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-03-11 17:06:25 +0000

    Mon, Mar 11, 2024, 10:06 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -3,15 +3,24 @@ open = document.getElementById('open')
 modal = document.getElementById('modal')
 close = document.getElementById('close')
 
-
+//toggle nav
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+// Show modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+// Hide modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+// Hide modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
