e9dac293f0561d6b060f502c381467b80f821f93
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-03-18 05:53:05 +0000

    Sun, Mar 17, 2024, 10:53 PM -07:00

---
+++ js/test.js
@@ -0,0 +1,23 @@
+h3ader = document.querySelectorAll('h3')
+
+h3ader.forEach(h3 => {
+    h3.addEventListener('mouseover', () => {
+        removeActiveClasses()
+        h3.classList.add('active')
+    h3.addEventListener('mouseout', (removeActiveClasses))
+
+    })
+})
+
+if (h3.active = true)
+{
+    
+}
+function addActiveClasses(){
+    h3ader.forEach(h3 => {})
+}
+function removeActiveClasses(){
+    h3ader.forEach(h3 => {
+        h3.classList.remove('active')
+    })
+}
